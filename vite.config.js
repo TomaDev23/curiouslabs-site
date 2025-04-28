f05a27e6954e2c03cb3a7f42376e062e8bd0b4e4
TomaDev23 <towcambodia@gmail.com>
2025-04-28 11:23:03 +0700

    chore: fix vite config and relocate index.html to public/

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",   // 🔥 Needed so Vite relative paths resolve in dist
+  build: {
+    outDir: "dist",
+  },
 })
